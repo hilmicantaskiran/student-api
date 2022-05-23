b9607dccb03d329e9790eab9824e2c9a02915c83
Hilmi Can Taşkıran <hilmicantaskiran@gmail.com>
2022-05-24 00:32:01 +0300

    deleted api folder

--- api/utils/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-module.exports.connect = async () => {
-  await mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  });
-};
-
